51c8718dd1140d7c407fd747cd669a67afadeaa8
Mareko James <mmj342@nyu.edu>
2024-06-01 00:31:42 -0400

    Inital Commit

---
+++ functions/index.js
@@ -0,0 +1,29 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+
+const { PKPass } = require("passkit-generator");
+var fs = require('file-system');
+var path = require('path');
+var axios = require('axios');
+
+exports.pass = functions.https.onRequest((request, response) => {
+    functions.logger.info("Hello logs!", {structredData: true});
+    response.load("Hello from Firebase!");
+})
+
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
